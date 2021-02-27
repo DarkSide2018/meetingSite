d5850c073e92e95331cde67422d593236b7315ad
DarkSide <romangaponov@gmail.com>
2021-02-27 15:03:56 +0300

    initial commit

---
+++ src/main/java/com/meeting/site/meetingSite/MeetingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.site.meetingSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingSiteApplication.class, args);
+	}
+
+}
