66e9a6f2dc1d062eaaf34b7cbd8b1d258912a578
DarkSide <romangaponov@gmail.com>
2021-03-05 09:20:50 +0300

    add environment

--- src/main/java/com/meeting/site/meetingSite/service/ScheduleService.java
+++ src/main/java/com/meeting/site/meetingSite/service/ScheduleService.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @Service
 public class ScheduleService {
@@ -17,15 +17,23 @@ public class ScheduleService {
     @Autowired
     @Qualifier("threadPoolTaskExecutor")
     private ThreadPoolTaskExecutor taskExecutor;
+
     @Autowired
     private Job job;
 
     @Scheduled(fixedRate = 1000)
     public void schedule(){
+         int threadCount = 0;
         System.out.println("poolSize - > " + taskExecutor.getPoolSize());
         System.out.println("activeCount - > " + taskExecutor.getActiveCount());
         System.out.println("queueSize - > " + taskExecutor.getThreadPoolExecutor().getQueue().size());
         gateWay.setUuid(UUID.randomUUID());
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        for ( Thread t : threadSet){
+            System.out.println("Thread :"+t+":"+"state:"+t.getState());
+                ++threadCount;
+        }
+        System.out.println("Thread count started by Main thread:"+threadCount);
         taskExecutor.execute(gateWay);
         job.acyncJob(UUID.randomUUID());
     }
---
+++ src/test/java/com/meeting/site/meetingSite/MeetingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meeting.site.meetingSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
