ce8339968536af48ea8085dfa9fdb49eb449bab3
DarkSide <romangaponov@gmail.com>
2021-03-08 00:38:13 +0300

    add router example

--- frontend/app-react/src/js/client.js
+++ frontend/app-react/src/js/client.js
@@ -1,10 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import {Provider} from "react-redux"
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
 
 import Main from "./components/Main.jsx"
-
 
 
 import store from "./store"
@@ -15,10 +14,15 @@ require('file-loader?name=index.html!../index.html');
 const app = document.getElementById('app')
 
 ReactDOM.render(<Provider store={store}>
-  <Router history={hashHistory}>
-      <IndexRoute component={Main}/>      
-      <Route path="/main" name="main" component={Main}></Route>
-    <IndexRoute component={Routing}/>
-    <Route path="/routing" name="routing" component={Routing}></Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={Main}/>
+            <Route path="main" name="main" component={Main}></Route>
+        </Route>
+        <Route path="/register" component={Routing}>
+            <IndexRoute component={Routing}/>
+            <Route path="/register" name="register" component={Routing}></Route>
+        </Route>
+
+    </Router>
 </Provider>, app);
--- frontend/app-react/src/js/components/Login.jsx
+++ frontend/app-react/src/js/components/Login.jsx
@@ -1,111 +1,128 @@
 import React from "react";
-import { connect } from "react-redux"
+import {connect} from "react-redux"
 
 
-import { Form } from 'react-bootstrap';
-import { FormGroup } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import { FormControl } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { ControlLabel } from 'react-bootstrap';
-import { Checkbox } from 'react-bootstrap';
-import { Panel } from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import {FormGroup} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {ControlLabel} from 'react-bootstrap';
+import {Checkbox} from 'react-bootstrap';
+import {Panel} from 'react-bootstrap';
 
 
-
-
-import { doLogin } from "../actions/loginActions";
+import {doLogin} from "../actions/loginActions";
+import Link from "react-router";
 
 
 @connect((store) => {
-  return {
-    loginFailed: store.login.failed
-  };
+    return {
+        loginFailed: store.login.failed
+    };
 })
 
 
 class Login extends React.Component {
 
 
- constructor(props) {
-    super(props);    
-    this.state = {
-      login: "",
-      pass: ""
-    };
-  }
-
-  pressEnter(event) {
-  	if(event.charCode == 13){
-          this.doLogin();    
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: "",
+            pass: ""
+        };
     }
-  }
 
-  changeEmail(event) {		 
-    let fleldVal = event.target.value;    
-    this.setState({login: fleldVal, pass : this.state.pass});
-  }
-  
-  changePass(event) {
-	let fleldVal = event.target.value;
-	this.setState({pass: fleldVal, login : this.state.login});    
-  }
+    pressEnter(event) {
+        if (event.charCode == 13) {
+            this.doLogin();
+        }
+    }
 
-  doLogin() {  	
-    this.props.dispatch(doLogin(this.state.login, this.state.pass))
-  }
+    changeEmail(event) {
+        let fleldVal = event.target.value;
+        this.setState({login: fleldVal, pass: this.state.pass});
+    }
 
-  render() {  	
+    changePass(event) {
+        let fleldVal = event.target.value;
+        this.setState({pass: fleldVal, login: this.state.login});
+    }
 
-	const { loginFailed } = this.props;
+    doLogin() {
+        this.props.dispatch(doLogin(this.state.login, this.state.pass))
+    }
 
-	const formBox = {	    
-    	marginTop: '15%',
-    	marginLeft: '30%', 
-    	width : '30%',
-	};	
-	
-	const btn = {
-	    width: '100%',
-	}
+    doRegister() {
+        this.props.dispatch(doLogin(this.state.login, this.state.pass))
+    }
 
-	const title = "Login Page" ;
-    return (
-    <div>   
-      <Panel header={title} bsStyle="primary" style={formBox}>
-      		<div>	    	   
-			      <Form horizontal>
-				    <FormGroup controlId="formHorizontalEmail">
-				      <Col componentClass={ControlLabel} sm={1}>			        
-				      </Col>
-				      <Col sm={10}>
-				        <FormControl type="email" placeholder="Login" name="login" onChange={this.changeEmail.bind(this)} onKeyPress={this.pressEnter.bind(this)}/>
-				      </Col>
-				    </FormGroup>
+    render() {
 
-				    <FormGroup controlId="formHorizontalPassword">
-				      <Col componentClass={ControlLabel} sm={1}>
-				        
-				      </Col>
-				      <Col sm={10}>
-				        <FormControl type="password" placeholder="password" name="pass" onChange={this.changePass.bind(this)} onKeyPress={this.pressEnter.bind(this)}/>
-				      </Col>
-				    </FormGroup>				    
+        const {loginFailed} = this.props;
 
-				    <FormGroup>
-				      <Col smOffset={1} sm={10}>
-				        <Button bsStyle="primary"  onClick={this.doLogin.bind(this)} style={btn} >
-				          Sign In
-				        </Button>
-				      </Col>
-				    </FormGroup>
-				  </Form>
-			  </div>
-       </Panel> 	    	
+        const formBox = {
+            marginTop: '15%',
+            marginLeft: '30%',
+            width: '30%',
+        };
 
-     </div>
-    );
-  }
+        const btn = {
+            width: '100%',
+        }
+
+        const title = "Login Page";
+        return (
+            <div>
+                <Panel header={title} bsStyle="primary" style={formBox}>
+                    <div>
+                        <Form horizontal>
+                            <FormGroup controlId="formHorizontalEmail">
+                                <Col componentClass={ControlLabel} sm={1}>
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="email" placeholder="Login" name="login"
+                                                 onChange={this.changeEmail.bind(this)}
+                                                 onKeyPress={this.pressEnter.bind(this)}/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={1}>
+
+                                </Col>
+                                <Col sm={10}>
+                                    <FormControl type="password" placeholder="password" name="pass"
+                                                 onChange={this.changePass.bind(this)}
+                                                 onKeyPress={this.pressEnter.bind(this)}/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <div>
+                                    <Col smOffset={1} sm={10}>
+                                        <Button bsStyle="primary" onClick={this.doLogin.bind(this)} style={btn}>
+                                            Sign In
+                                        </Button>
+                                    </Col>
+                                </div>
+                                <div>
+                                    <Col smOffset={1} sm={10}>
+                                        <hr/>
+                                        <Button bsStyle="primary" onClick={this.doRegister.bind(this)} style={btn}>
+                                            Register
+                                        </Button>
+                                    </Col>
+                                </div>
+                            </FormGroup>
+                        </Form>
+                    </div>
+                </Panel>
+
+            </div>
+        );
+    }
 }
 
 
